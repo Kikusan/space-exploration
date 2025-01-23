b83d0ebc8ac63a308986ad7ca7ae4ac1c3c60844
Huu Thang NGUYEN <huu-thang.nguyen@socgen.com>
2025-01-23 18:23:23 +0100

    chore: test cockpit page header

---
+++ frontend/src/pages/Cockpit/components/header/header.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Header } from './index.tsx';
+import { vi, describe, it, expect } from 'vitest';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Mock de l'importation du fichier SVG
+vi.mock('@assets/icon-spaceship.svg?react', () => ({
+  default: 'svg',
+}));
+
+describe('Header component', () => {
+  it('should navigate to spaceship-admin page when the button is clicked', () => {
+    render(
+      <Router>
+        <Header />
+      </Router>,
+    );
+
+    const button = screen.getByText(/Spaceship admin/i);
+    fireEvent.click(button);
+    expect(window.location.pathname).toBe('/spaceship-admin');
+  });
+});
